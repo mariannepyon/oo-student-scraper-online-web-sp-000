8256fea1c89f899d3a0c782e44c3fe7ab4e10705
Marianne Pyon <mariannepyon@gmail.com>
2019-09-19 13:41:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @@all<<self
   end
 
   def self.create_from_collection(students_array)
@@ -13,11 +13,10 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+    @@all
   end
 end
-
